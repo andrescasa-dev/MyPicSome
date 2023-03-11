009b83815a0e1816e4434c566e79ed810896326c
Andres <andrescasa.dev@gmail.com>
2023-03-11 12:26:43 -0500

    70% landing

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,17 @@
 const App = props => {
   return (
-    <>
-      <h1>app</h1>
-    </>
+    <main className="font-sans">
+      <section className="grid justify-center items-start h-screen bg-half-square after:bg-blue-500">
+        <h1 className="self-end p-9 text-center text-6xl text-linear font-bold drop-shadow-sm">Save & Buy Pictures</h1>
+        <div className="flex flex-wrap justify-center items-start gap-10 p-9 px-16">
+          <p className="max-w-[25ch] text-lg font-light text-center text-sky-900">Buy and save your favorites pictures and bring your vision to life. Start exploring now!</p>
+          <a className="flex px-12 py-4 gap-4 rounded-md text-xl font-bold text-white bg-sky-700" href="#">
+            Explore
+            <img src="./assets/arrow.svg"/>
+          </a>
+        </div>
+      </section>
+    </main>
   )
 }
 
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -5,7 +5,14 @@ module.exports = {
     './src/**/*.{js,ts,jsx,tsx}'
   ],
   theme: {
-    extend: {}
+    fontFamily: {
+      sans: 'Merriweather Sans'
+    },
+    extend: {
+      gridTemplateColumns: {
+        main: 'minmax(1rem, 1fr) repeat(4, 77px) minmax(1rem, 1fr)'
+      }
+    }
   },
   plugins: []
 }
